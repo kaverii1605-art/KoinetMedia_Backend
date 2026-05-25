6f9bd4e3c68467419586198e0a5d393643c0582a
kaverii1605-art <kaverii1605@gmail.com>
2026-05-25 14:49:24 +0530

    updated

--- src/main/java/com/koinetmedia/KoinetMedia/security/JwtFilter.java
+++ src/main/java/com/koinetmedia/KoinetMedia/security/JwtFilter.java
@@ -77,6 +77,7 @@ public class JwtFilter extends OncePerRequestFilter {
             return;
         }
 
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(request, response)
+        ;
     }
 }
